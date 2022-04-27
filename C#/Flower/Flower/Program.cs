09f40f7775c5cb7e13cb126eb8e3b8f93c61d9e1
Lokesh Bomma <l.bomma@zensar.com>
2022-04-27 16:15:08 +0530

    Assign4

---
+++ C#/Flower/Flower/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flower
+{
+    internal class Program
+    {
+        public class flower1
+        {
+            public String name;
+            public String color;
+            public flower1(String name,String color)
+            {
+                this.name = name;
+                this.color = color;
+                Console.WriteLine($"name is {name} & color is {color}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            flower1 f = new flower1("datura","red");
+            Console.ReadLine();
+        }
+    }
+}
