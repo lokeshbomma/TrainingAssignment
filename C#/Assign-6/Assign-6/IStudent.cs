028970944627e1e12ca399a0461f5dce634ac031
Lokesh Bomma <l.bomma@zensar.com>
2022-05-04 17:24:07 +0530

    Assign-6

---
+++ C#/Assign-6/Assign-6/IStudent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_6
+{
+    interface IStudent
+    {
+        void DisplayDetails(int Id, string Name);
+    }
+}
---
+++ C#/Assign-6/Assign-6/InterfaEg.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_6
+{
+    class InterfaEg
+    {
+        static void Main()
+        {
+
+            IStudent obj = new DayScholar();
+            obj.DisplayDetails(1, "vamsikrishna");
+
+
+            IStudent obj1 = new Resident();
+            obj1.DisplayDetails(2, "Krishna vamsi");
+
+            Console.ReadLine();
+        }
+    }
+}
